51f3541f0e5610fd38a9f949d764f7ed0877b26b
Akileshdash <119668304+Akileshdash@users.noreply.github.com>
2024-10-03 21:30:20 +0530

    feat : news block and discussion forum pages initiated

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,8 +8,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-
-      <Navbar/>
+        <Navbar/>
         <main>{children}</main>
         <Footer />
       </body>
