1ff1bdfa733681c02c7e8c546be04b2093a9104a
StompingJalapeno <preetbobde292@gmail.com>
2024-10-08 01:17:35 +0530

    Updates to selected files

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Welcome to About page 😎</div>
+    <div></div>
   )
 }
 
--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>Welcome to Contact page 😊</div>
+    <div> </div>
   )
 }
 
