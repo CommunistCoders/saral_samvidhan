ac3be209997bef0d4e718c14341646f86cb5d63c
Lav437 <lavkushjnv437@gmail.com>
2024-11-03 22:42:34 +0530

    Ui change in discussion forum and hero section

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -4,45 +4,47 @@
 import React, { useState, useEffect } from "react";
 import { useSession, signOut } from "next-auth/react";
 import Link from "next/link";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { FaBars } from 'react-icons/fa6';
+import { FaTimes } from 'react-icons/fa';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const { data: session } = useSession(); // Get session data
 
-  useEffect(() => {
-    // Load Google Translate script once
-    const addGoogleTranslateScript = () => {
-      if (!window.google || !window.google.translate) {
-        const script = document.createElement("script");
-        script.type = "text/javascript";
-        script.src =
-          "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
-        document.body.appendChild(script);
+  // useEffect(() => {
+  //   // Load Google Translate script once
+  //   const addGoogleTranslateScript = () => {
+  //     if (!window.google || !window.google.translate) {
+  //       const script = document.createElement("script");
+  //       script.type = "text/javascript";
+  //       script.src =
+  //         "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+  //       document.body.appendChild(script);
 
-        window.googleTranslateElementInit = () => {
-          new window.google.translate.TranslateElement(
-            { pageLanguage: "en" },
-            "google_translate_element"
-          );
-        };
-      }
-    };
+  //       window.googleTranslateElementInit = () => {
+  //         new window.google.translate.TranslateElement(
+  //           { pageLanguage: "en" },
+  //           "google_translate_element"
+  //         );
+  //       };
+  //     }
+  //   };
 
-    addGoogleTranslateScript();
-  }, []);
+  //   addGoogleTranslateScript();
+  // }, []);
 
   const handleTranslateClick = () => {
     setIsOpen((prev) => !prev);
   };
 
   return (
-    <nav className="bg-white shadow-md sticky top-0 z-50">
-      <div className="max-w-7xl mx-auto px-4  mt-5 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center h-16">
-          {/* Logo */}
-          <div id="google_translate_element" className="mt-5"></div>
+    <nav className=" bg-black fixed left-0 right-0 top-2  border border-amber-200/30 rounded-lg z-50">
+      <div className="max-w-7xl mx-auto px-4  sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16 gap-6">
+         
           <div className="flex-shrink-0">
-            <Link href="/" className="text-2xl font-bold text-blue-600">
+            <Link href="/" className="text-3xl font-bold text-amber-400">
               Samvidhan
             </Link>
            
@@ -52,85 +54,47 @@ const Navbar = () => {
           {/* Links and Search Form */}
 
           <div className="hidden md:flex space-x-4 items-center">
-            <Link href="/" className="text-gray-700 hover:text-blue-600">
-              Home
+            <Link href="/" className="text-stone-50 hover:text-amber-600">
+              HOME
             </Link>
-            <Link href="/Chatbot" className="text-gray-700 hover:text-blue-600">
-              Chatbot
+            <Link href="/Chatbot" className="text-stone-50 hover:text-amber-600">
+              CHATBOT
             </Link>
-            <Link href="/discussionforum" className="text-gray-700 hover:text-blue-600">
+            <Link href="/discussionforum" className="text-stone-50 hover:text-amber-600">
               Discussion Forum
             </Link>
-            {/* <Link href="/glossary" className="text-gray-700 hover:text-blue-600">
-            Glossary
-            </Link> */}
-            <Link href="/news" className="text-gray-700 hover:text-blue-600">
+            <Link href="/news" className="text-stone-50 hover:text-amber-600">
               News
             </Link>
-            <Link href="/game" className="text-gray-700 hover:text-blue-600">
+            <Link href="/game" className="text-gray-50 hover:text-amber-600">
               Game
             </Link>
             {session ? ( // Conditionally render logout button
               <button
                 onClick={() => signOut()}
-                className="text-gray-700 hover:text-blue-600"
+                className="text-stone-50 hover:text-amber-600"
               >
                 Logout
               </button>
             ) : (
-              <Link href="/login" className="text-gray-700 hover:text-blue-600">
+              <Link href="/login" className="text-stone-50 hover:text-amber-600">
                 Login
               </Link>
             )}
             <Link
               href="/lawchronicles"
-              className="text-gray-700 hover:text-blue-600"
+              className="text-stone-50 hover:text-amber-600"
             >
               Law Chronicles
             </Link>
 
-            {/* Language Selector - Globe Icon */}
-
-            <button
-              onClick={handleTranslateClick}
-              className="text-blue-700 hover:text-blue-800 flex items-center"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 512 512"
-                className="h-6 w-6"
-              >
-                <path
-                  fill="#1C64F2"
-                  d="M352 256c0 22.2-1.2 43.6-3.3 64l-185.3 0c-2.2-20.4-3.3-41.8-3.3-64s1.2-43.6 3.3-64l185.3 0c2.2 20.4 3.3 41.8 3.3 64zm28.8-64l123.1 0c5.3 20.5 8.1 41.9 8.1 64s-2.8 43.5-8.1 64l-123.1 0c2.1-20.6 3.2-42 3.2-64s-1.1-43.4-3.2-64zm112.6-32l-116.7 0c-10-63.9-29.8-117.4-55.3-151.6c78.3 20.7 142 77.5 171.9 151.6zm-149.1 0l-176.6 0c6.1-36.4 15.5-68.6 27-94.7c10.5-23.6 22.2-40.7 33.5-51.5C239.4 3.2 248.7 0 256 0s16.6 3.2 27.8 13.8c11.3 10.8 23 27.9 33.5 51.5c11.6 26 20.9 58.2 27 94.7zm-209 0L18.6 160C48.6 85.9 112.2 29.1 190.6 8.4C165.1 42.6 145.3 96.1 135.3 160zM8.1 192l123.1 0c-2.1 20.6-3.2 42-3.2 64s1.1 43.4 3.2 64L8.1 320C2.8 299.5 0 278.1 0 256s2.8-43.5 8.1-64zM194.7 446.6c-11.6-26-20.9-58.2-27-94.6l176.6 0c-6.1 36.4-15.5 68.6-27 94.6c-10.5 23.6-22.2 40.7-33.5 51.5C272.6 508.8 263.3 512 256 512s-16.6-3.2-27.8-13.8c-11.3-10.8-23-27.9-33.5-51.5zM135.3 352c10 63.9 29.8 117.4 55.3 151.6C112.2 482.9 48.6 426.1 18.6 352l116.7 0zm358.1 0c-30 74.1-93.6 130.9-171.9 151.6c25.5-34.2 45.2-87.7 55.3-151.6l116.7 0z"
-                />
-              </svg>
-              <span className="ml-2">Translate</span>
-            </button>
-
-            {/* Google Translate Dropdown */}
           </div>
 
           {/* Hamburger Menu for Mobile */}
           <div className="-mr-2 flex md:hidden">
             <button
-              onClick={() => setIsOpen(!isOpen)}
-              className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
-            >
-              <svg
-                className="w-6 h-6"
-                fill="none"
-                stroke="currentColor"
-                viewBox="0 0 24 24"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16m-7 6h7"}
-                ></path>
-              </svg>
+              onClick={() => setIsOpen(!isOpen)} className="text-white">
+              {isOpen ? <FaTimes className='m-2 h-6 w-5' /> : <FaBars className='m-2 h-6 w-5'/>}
             </button>
           </div>
         </div>
@@ -141,35 +105,29 @@ const Navbar = () => {
         <div className="md:hidden px-2 pt-2 pb-3 space-y-1 sm:px-3">
           <Link
             href="/"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-white hover:bg-blue-600"
+            className="block px-3 py-2 rounded-md text-base font-medium text-stone-50  hover:bg-amber-600"
           >
             Home
           </Link>
           <Link
             href="/discussionforum"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-white hover:bg-blue-600"
+            className="block px-3 py-2 rounded-md text-base font-medium text-stone-50  hover:bg-amber-600"
           >
             Discussion Forum
           </Link>
-          {/* <Link
-            href="/glossary"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-white hover:bg-blue-600"
-          >
-          Glossary
-          </Link> */}
           <Link
             href="/contact"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-white hover:bg-blue-600"
+            className="block px-3 py-2 rounded-md text-base font-medium text-stone-50 hover:bg-amber-600"
           >
             Contact
           </Link>
           <Link
             href="/login"
-            className="block px-3 py-2 rounded-md text-base font-medium text-gray-700 hover:text-white hover:bg-blue-600"
+            className="block px-3 py-2 rounded-md text-base font-medium text-stone-50 hover:bg-amber-600"
           >
             Login
           </Link>
-          <div id="google_translate_element"></div>
+         
         </div>
       )}
 
