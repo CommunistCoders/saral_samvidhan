ab19cd4a684b34d6b417d848b11ab9bc6da61e9b
Lav437 <lavkushjnv437@gmail.com>
2024-11-13 17:36:25 +0530

    fixed language change and ui

---
+++ src/app/components/TranslateComponent.jsx
@@ -0,0 +1,37 @@
+import { use } from 'passport'
+import React, { useEffect, useRef } from 'react'
+
+const TranslateComponent = () => {
+
+    const googleTranslateRef = useRef(null);
+
+    useEffect(() => {
+        let intervalId;
+
+        const checkGoogleTranslate = () => {
+            if (window.google && window.google.translate) {
+                clearInterval(intervalId);
+                new window.google.translate.TranslateElement(
+                    {
+                        pageLanguage: 'en', layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+                    },
+                    googleTranslateRef.current
+                );
+            }
+        };
+
+        intervalId = setInterval(checkGoogleTranslate, 100);
+
+    }, []);
+
+return (
+    
+        <div>
+            <div ref={googleTranslateRef}  className='flex flex-row justify-center' > 
+            </div>
+        </div>
+    
+)
+}
+
+export default TranslateComponent
